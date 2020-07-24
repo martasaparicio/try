cdd1adf325e6633f3c7e6485b72ddf8a8e8e2549
Marta Aparício <36605729+martasaparicio@users.noreply.github.com>
2020-07-24 16:52:02 +0100

    Update DepositPaying.sol

--- DepositPaying.sol
+++ DepositPaying.sol
@@ -3,36 +3,37 @@ pragma solidity >=0.4.22 <0.7.0;
 
 import "./Transaction.sol";
 import "./RentalCompleting.sol";
+//import "./CarTaking.sol";
 
 contract DepositPaying is Transaction {
     
-    RentalCompleting rentalCompleting;
+    RentalCompleting public rentalCompleting;
+    CarTaking public carTarking;
 
     constructor(address _rentalCompleting) public{
-        initiator =  msg.sender; //client
-        executor = 0x23ED60DEc9490AEBA10f9C36ED25Ae62FF0Ea216; //rentACar
+        initiator = msg.sender; //client   msg.sender                            
+        executor = 0xf57596949E8ee597e4D1464706a49CD71FB25AdF; //rentACar
         
         rentalCompleting = RentalCompleting(_rentalCompleting);
     }
     function getCarGroup() view public returns (RentalCompleting.CarGroup memory){
         return rentalCompleting.returnsCarGroup(address(this));
     }
-     function getRental() view public returns (RentalCompleting.Rental memory){
+    function getRental() view public returns (RentalCompleting.Rental memory){
         return rentalCompleting.returnsRental(address(this));
     }
-    //RentalCompleting.CarGroup  cg = rentalCompleting.returnsCarGroup(address(this));
-    //RentalCompleting.Rental r = rentalCompleting.returnsRental(address(this));
     
     function requestDepositPaying() public
              atCFact(C_facts.Inital)
              onlyBy(executor){
-             //require(cg.standardDepositAmount == 100);
+             //require(getCarGroup().standardDepositAmount == getRental().depositAmount);COMO SE ACEDE
                  c_fact = C_facts.Requested;
              }
     
     function promiseRentalCompleting() public
              atCFact(C_facts.Requested)
              onlyBy(initiator){
+                 
                  c_fact = C_facts.Promissed;
              }
     
@@ -43,20 +44,25 @@ contract DepositPaying is Transaction {
                  c_fact = C_facts.Inital;
              }
              
-    function declareRentalCompleting() payable public
+    function declareRentalCompleting() public
              atCFact(C_facts.Promissed)
              onlyBy(initiator)
              p_act(){
-                //require(RentalCompleting.rental.depositAmount == msg.value); 
+                require(getCarGroup().standardDepositAmount == getRental().depositAmount); //NOT SURE...
                 
                 c_fact = C_facts.Declared; 
              }
              
     function acceptRentalCompleting() public
              atCFact(C_facts.Declared)
-             onlyBy(executor){
+             onlyBy(executor)
+             returns (address){
+                 
+                 carTarking = new CarTaking(address(this));
                  
                  c_fact = C_facts.Accepted; 
+                 
+                 return address(carTarking);
              }
              
     function rejectRentalCompleting() public
