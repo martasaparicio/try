900beec679d1b34198506ed09c147aacfb41c44d
Marta Aparício <36605729+martasaparicio@users.noreply.github.com>
2020-12-20 17:31:11 +0000

    Update ElectionControlling.sol

--- UseCases/EUPE/ElectionControlling.sol
+++ UseCases/EUPE/ElectionControlling.sol
@@ -1,1 +1,154 @@
+pragma experimental ABIEncoderV2;
+pragma solidity >=0.4.22 <0.7.0;
 
+import "./Transaction.sol";
+import "./ElectionCompleting.sol";
+import "./PoliticalPartyRegistering.sol";
+import "./CandidateRegistering.sol";
+import "./ElectionVoteCompleting.sol";
+import "./ElectionVoting.sol";
+import "./ElectionVoteCounting.sol";
+import "./SeatAssigning.sol";
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/token/ERC721/ERC721.sol";
+
+
+contract VotingToken is Ownable, ERC721{ 
+	constructor(string memory name, string memory symbol) ERC721(name, symbol) Ownable() public payable{
+	}
+
+	function mint(address receiver) onlyOwner public {
+		_safeMint(receiver, uint256(receiver));
+	}
+
+	function transfer(address from, address to) onlyOwner public {
+		_transfer(from, to, uint256(from));
+	}
+
+ }
+ 
+ contract ElectionControlling is Transaction{
+    
+    uint256 approveCandidatesCounter; 
+    
+    VotingToken votingToken;
+    
+    uint candidateApprovalEnd;
+
+	  uint candidateRegistrationEnd;
+
+	  uint partyRegistrationEnd;
+
+	  uint startDate;
+
+	  mapping (address => Candidate) candidates;
+	  address[] candidatesKeys;
+
+	  mapping (address => PoliticalParty) politicalParties;
+	  address[] politicalPartiesKeys;
+
+	  CountryElections[] countries;
+
+	  enum VotingSystem{
+		  ClosedList,
+		  OpenList,
+		  SingleTransferable
+	  }
+
+	  struct CountryElections{
+		  uint256 id;
+		  string countryName;
+		  uint electionBeginDate;
+		  uint electionEndDate;
+		  address[] voters;
+		  VotingSystem votingSystem;
+		  int availableSeats;
+		  uint8 electoralTreshold;
+		  uint8 minimumAge;
+	  }
+
+	  struct PoliticalParty{
+		  address id;
+		  string name;
+		  string code;
+		  string website;
+		  int voteCount;
+		  int allocatedSeats;
+		  uint256 countryId;
+	  }
+
+	  struct Candidate{
+		  address id;
+		  string name;
+		  string website;
+		  int voteCount;
+		  bool hasSeat;
+		  bool approved;
+		  address partyId;
+	  }
+    
+    
+    constructor() public{
+        initiator = 0x588086ae79BD939160A73B939aB2CcDD366bE76B; 
+        executor = msg.sender;
+        
+        votingToken = new VotingToken("VotingToken","VOTE");
+        candidateApprovalEnd = 9598986017;
+	      candidateRegistrationEnd = 9598986017;
+	      partyRegistrationEnd = 9598986017;
+	    
+		    CountryElections memory czechElections = CountryElections(0,"Czech Republic", 9598986017, 959994017, new address[](0), VotingSystem.OpenList, 10, 20, 18);
+        countries.push(czechElections);
+        CountryElections memory germanElections = CountryElections(1,"Germany", 9598986017, 959994017, new address[](0), VotingSystem.ClosedList, 20, 40, 18);
+        countries.push(germanElections);
+    }
+    
+    function requestElectionControlling() public
+             atCFact(C_facts.Inital)
+             onlyBy(executor)
+             transitionNext(true){
+                
+            }
+             
+    function promiseElectionControlling() public
+             atCFact(C_facts.Requested)
+             onlyBy(initiator)
+             transitionNext(true)
+             returns (address){
+             
+             ElectionCompleting electionCompleting = new ElectionCompleting(address(this));
+
+             return address(electionCompleting);
+                
+             }
+    
+    function declineElectionControlling() public
+             atCFact(C_facts.Requested)
+             onlyBy(initiator)
+             transitionNext(false){
+                 
+             }
+
+    function declareElectionControlling(address _invoicePaying) public
+             atCFact(C_facts.Promissed)
+             onlyBy(initiator)
+             p_act()
+             transitionNext(true){
+                                 
+             }
+             
+    function acceptElectionControlling() public
+             atCFact(C_facts.Declared)
+             onlyBy(executor)
+             transitionNext(true){
+                  
+             }
+             
+    function rejectElectionControlling() public
+             atCFact(C_facts.Declared)
+             onlyBy(executor)
+             transitionNext(false){
+                 
+             }
+}
